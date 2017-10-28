d04d23186e0f0b2d8dc03d7d725f7806b711302a
Michael <690566870@qq.com>
2017-10-28 23:57:40 +0800

    update 1.1.2

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /* server starts */
 require('./core/core');
-config.rootDirectory = __dirname;
-config.vhost = require('./config/vhost.config');
+config.rootDirectory = `${__dirname}../../`;
 config.route = require('./config/route.config');
 config.server = require('./config/server.config');
