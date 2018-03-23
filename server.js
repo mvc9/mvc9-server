2e09105cbf2e8c2010a77e77daf8cf8aef69589c
Michael <funplace@qq.com>
2018-03-23 14:31:19 +0800

    update 2018.3.23

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 /* server starts */
+
+/* this file only for node_modules import or require */
 require('./core/core');
 config.rootDirectory = `${__dirname}../../`;
-config.route = require('./config/route.config');
-config.server = require('./config/server.config');
+
+/* you should create and require your config files at your project root path */
+/* for example */
+// config.vhost = require('./config/vhost.config');
+// config.route = require('./config/route.config');
+// config.server = require('./config/server.config');
+
+/* and then you can start your server now */
+// startServer();
