ff265bddc485d67a2804ed0e7d87ba7b08419229
sunny <funplace@qq.com>
2022-08-23 18:19:30 +0800

    some new;

--- core/resource-router.js
+++ core/resource-router.js
@@ -1,71 +1,39 @@
-const router = {};
+/* map file generate resource map */
 
-router.toResFolder = (domain, port, vhostList) => {
-  const parsePort = parseInt(port.replace(':', ''));
-  let folder = '';
-  vhostList.map((vhost, index) => {
-    if (domain === vhost.Domain && parsePort === vhost.Port) {
-      folder = vhost.RootFolder;
-    }
-    if (domain === vhost.Domain2 && parsePort === vhost.Port) {
-      folder = vhost.RootFolder;
-    }
-  })
-  return folder;
-}
+const fileSystem = require('fs');
 
-router.toRes = (pathname, rootPath, routeConfig) => {
-  const matchPath = (pathName) => {
-    const pathnameNoParamRegex = new RegExp('^(/(?!p/)[^/]+)+', 'g');
-    const pathNameNoParam = (pathName.match(pathnameNoParamRegex) || ['/'])[0];
-    const regExpPath = new RegExp('/([^\\/]+/)+');
-    const regExpSuffix = new RegExp('[^\\.]+$');
-    const pathSplit = (pathNameNoParam.match(regExpPath) || [])[0] || '/';
-    const paths = pathNameNoParam.split(pathSplit);
-    const target = paths[paths.length - 1] || 'index';
-    const targetSuffix = target.match(regExpSuffix) || [];
-    return {
-      type: 'file',
-      rootPath: rootPath,
-      path: pathSplit,
-      target: target,
-      targetSuffix: targetSuffix[0]
-    };
-  }
+const resRouter = {};
 
-  let resLink = matchPath(pathname);
-  if (resLink.targetSuffix !== resLink.target) {
-    if (resLink.target.match(new RegExp(`\\.${routeConfig.controllerSuffix}`))) {
-      resLink.type = 'controller';
-    }
-  } else {
-    Object.keys(routeConfig.redirect || {}).map((matchKey) => {
-      const matchRegex = new RegExp(matchKey);
-      if (matchRegex.test(pathname)) {
-        resLink = matchPath(routeConfig.redirect[matchKey]);
+resRouter.routes = {};
+
+resRouter.mapFileSync = (dirPath, charset = 'UTF-8') => {
+  const fileList = [];
+  const mapDirFiles = (dirPath) => {
+    const dirFiles = fs.readdirSync(dirPath);
+    dirFiles.map((fileName) => {
+      const filePath = path.join(dirPath, fileName);
+      if (fs.statSync(filePath).isDirectory()) {
+        mapDirFiles(filePath);
+      } else {
+        const fileData = fs.readFileSync(filePath, charset);
+        fileList.push({
+          fileName,
+          fileData,
+          filePath
+        });
       }
-    })
-    resLink.type = 'page';
+    });
   }
-  return resLink;
+  mapDirFiles(dirPath);
+  return fileList;
 }
 
-router.loadRes = (resLink) => {
-  switch (resLink.type) {
-    case 'page':
-      resLink.charset = config.server.Charset;
-      resLink.targetSuffix = 'html';
-      resLink.target === '/' ?
-      resLink.target = `index.${config.route.pageSuffix}` : resLink.target = `${resLink.target}.${config.route.pageSuffix}`;
-    case 'file':
-      resLink.charset = resLink.charset || config.server.FileCharset;
-      const fileData = modules.buffer.readRes(`${resLink.rootPath}${resLink.path}${resLink.target}`, resLink.charset);
-      return fileData === false ? false : fileData;
-      break;
-    case 'controller':
-      return false;
-  }
-  return 0;
-}
+resRouter.updateRoute = (path) => {
+  const file = 
+};
 
-module.exports = router;
+resRouter.updateRoutes = (paths) => {};
+
+resRouter.initRoutes = () => {};
+
+module.exports = resRouter;
